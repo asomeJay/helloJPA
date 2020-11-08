c1408b828778e48f56abb89061c5ee002e973fd3
asomejay <museum114@naver.com>
2020-11-08 23:05:21 +0900

    :sparkles: 1차 캐시 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,31 @@ public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        em.close();
-        emf.close();
 
+        EntityManager em = emf.createEntityManager();
+        EntityManager em2 = emf.createEntityManager();
+
+        Long beforeTime, afterTime;
+
+        try {
+            beforeTime = System.currentTimeMillis();
+            Member findMember = em.find(Member.class, 1L);
+            afterTime = System.currentTimeMillis();
+
+            System.out.println("findMember : " + (afterTime - beforeTime));
+
+            beforeTime = System.currentTimeMillis();
+            Member findMember2 = em2.find(Member.class, 2L);
+            afterTime = System.currentTimeMillis();
+
+            System.out.println("findMember2 : " + (afterTime - beforeTime));
+
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        } finally {
+            em.close();
+            em2.close();
+            emf.close();
+        }
     }
 }
